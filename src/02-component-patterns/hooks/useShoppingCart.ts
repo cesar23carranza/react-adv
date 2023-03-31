8969c25e16432796be3558c218d2c362cf8150dd
Cesar <cesar23carranza@gmail.com>
2023-03-30 18:26:53 -0600

    Control Props Alt

--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -8,34 +8,23 @@ export const useShoppingCart = () => {
     const [shoppingCart, setShoppingCart] = useState<{ [key:string]: ProductInCart }>({});
 
     const onProductCountChange = ({ count, product }: { count: number, product: Product} ) => {
-        // console.log(count, product);
+        
+        console.log({ count });
 
         setShoppingCart( oldShoppingCart => {
 
-            const productInCar: ProductInCart = oldShoppingCart[product.id] || { ...product, count: 0 };
+            
+            if( count === 0) {
 
-            if( Math.max( productInCar.count + count, 0 ) > 0) {
-                productInCar.count += count;
-                return {
-                    ...oldShoppingCart,
-                    [product.id]: productInCar
-                }
+                const { [product.id]: toDelete, ...rest } = oldShoppingCart;
+                return rest;
             }
 
-            const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-            return rest;
+            return {
+                ...oldShoppingCart,
+                [ product.id ]: { ...product, count}
 
-            // if( count === 0) {
-
-            //     const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-            //     return rest;
-            // }
-
-            // return {
-            //     ...oldShoppingCart,
-            //     [ product.id ]: { ...product, count}
-
-            // }
+            }
 
         })
         
